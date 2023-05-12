e4757c2861c7aa00e39df8611d1ef97c3cbe3329
mohannad <mohannad199344@gmail.com>
2023-05-12 18:55:17 +0300

    initialized localization

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:todo_app/home_layout/home_layout.dart';
 import 'package:todo_app/shared/styles/my_themes.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,6 +13,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('en'), // English
+        Locale('ar'), // Spanish
+      ],
+      locale: const Locale('en'),
       darkTheme: MyThemeData.darkThemeData,
       theme: MyThemeData.lightThemeData,
       themeMode: ThemeMode.light,
